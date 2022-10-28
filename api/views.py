dbce71fc01fc70bfb7d300ae9754860b3fc8b4d2
Osazee Odigie <osazee.odigie01@gmail.com>
2022-10-28 21:15:53 +0100

    Add runtime file

--- api/views.py
+++ api/views.py
@@ -4,9 +4,13 @@ from rest_framework import viewsets
 from .serializers import SlackUsersSerializer
 
 # Create your views here.
-class SlackUsersViewSet(viewsets.ModelViewSet):
-    # API endpoint that allows users to be viewed or edited.
+# class SlackUsersViewSet(viewsets.ModelViewSet):
+#     # API endpoint that allows users to be viewed or edited.
     
+#     queryset = SlackUsers.objects.all().order_by('-slackUsername')
+#     serializer_class = SlackUsersSerializer
+    
+class GetSlackUsers(RetrieveAPIView):
     queryset = SlackUsers.objects.all().order_by('-slackUsername')
     serializer_class = SlackUsersSerializer
 
--- apiproject/urls.py
+++ apiproject/urls.py
@@ -25,5 +25,6 @@ router.register(r'slackusers', views.SlackUsersViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('slackusers/<pk>', GetSlackUsers.as_view())
 ]

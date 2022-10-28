a59e87ef1ace745883b83d6ac2987fea39b8dd17
Osazee Odigie <osazee.odigie01@gmail.com>
2022-10-28 11:02:41 +0100

    Add runtime file

--- apiproject/settings.py
+++ apiproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(SECRET_KEY)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_on_heroku',
     
     # My Created Apps
     'api',

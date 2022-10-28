fc1d3c4d2f27f935b4262f7deeb31186269ea9ad
Osazee Odigie <osazee.odigie01@gmail.com>
2022-10-28 02:50:14 +0100

    Add all files

--- apiproject/settings.py
+++ apiproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+import django-heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,4 +135,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+django-heroku.settings(locals())

85f08cb1094d1a4ceaefd9725f5d21f61f7d1089
Osazee Odigie <osazee.odigie01@gmail.com>
2022-10-29 02:49:26 +0100

    Made changes

--- api/views.py
+++ api/views.py
@@ -15,7 +15,7 @@ def slackUser_list(request, format=None):
     
     slackUser = SlackUser.objects.all()
     serializer = SlackUserSerializer(slackUser, many=True)
-    return Response(serializer.data)[0]
+    return Response(serializer.data[0])
 
 # from rest_framework.generics import RetrieveAPIView
 

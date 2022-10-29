6fa51fb0fc1c068063c37731c2aa5fd7755898bf
Osazee Odigie <osazee.odigie01@gmail.com>
2022-10-29 02:34:19 +0100

    Made changes

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,20 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-from api.views import SlackUserViewSet
+# from django.urls import include, path
+# from rest_framework.routers import DefaultRouter
+# from api.views import SlackUserViewSet
 
-router = DefaultRouter()
-router.register('api', SlackUserViewSet)
+# router = DefaultRouter()
+# router.register('api', SlackUserViewSet)
+
+# urlpatterns = [
+#     path('', include(router.urls))
+# ]
+
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from api import views
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', views.slackUser_list)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from api.models import SlackUser
+# from rest_framework import viewsets
+# from .serializers import SlackUserSerializer
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from api.models import SlackUser
-from rest_framework import viewsets
-from .serializers import SlackUserSerializer
+from api.serializers import SlackUserSerializer
+
+
+@api_view(['GET'])
+def slackUser_list(request, format=None):
+    
+    slackUser = SlackUser.objects.all()
+    serializer = SlackUserSerializer(slackUser, many=True)
+    return Response(serializer.data)
 # from rest_framework.generics import RetrieveAPIView
 
 # Create your views here.
@@ -11,7 +25,7 @@ from .serializers import SlackUserSerializer
 #     queryset = SlackUser.objects.all()
 #     serializer_class = SlackUserSerializer
     
-class GetSlackUser(RetrieveAPIView):
-    serializer_class = SlackUserSerializer
-    objectset = SlackUser.objects.all()
+# class GetSlackUser(RetrieveAPIView):
+#     serializer_class = SlackUserSerializer
+#     objectset = SlackUser.objects.all()
 

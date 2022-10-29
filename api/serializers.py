4c951a0ac789992208774999e2ea425ff8b24b72
Osazee Odigie <osazee.odigie01@gmail.com>
2022-10-29 04:07:42 +0100

    Made changes

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from api.models import SlackUser
-from rest_framework_json_api import serializers
+from rest_framework import serializers
 
 class SlackUserSerializer(serializers.ModelSerializer):
     class Meta:

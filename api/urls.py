dd60bf53750ea121a7427fae18a6df21e0cffce2
Osazee Odigie <osazee.odigie01@gmail.com>
2022-10-29 04:12:25 +0100

    Made changes

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    path('', views.slackUser_list),
-    path('/redirect/', redirect_view)
+    path('', views.slackUser_list)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
